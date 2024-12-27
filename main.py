66b5b7b0b00fe644e54ad68545afac4d40eba251
bhattija5 <bhattija5@gmail.com>
2024-12-27 15:48:34 -0500

    asteroids addition working

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame, constants
+import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -9,42 +6,42 @@ from asteroidfield import AsteroidField
 
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	clock = pygame.time.Clock()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-	updatable = pygame.sprite.Group()
-	drawable = pygame.sprite.Group()
-	asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-	Asteroid.containters = (asteroids, updatable, drawable)
-	AsteroidField.containers = updatable
-	asteroid_field = AsteroidField()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
-	Player.containers = (updatable, drawable)
+    Player.containers = (updatable, drawable)
 
-	player = Player( SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-	dt = 0
+    dt = 0
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
+        for obj in updatable:
+            obj.update(dt)
 
-	while True:
-		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-       				return
+        screen.fill("black")
 
-		for obj in updatable:
-			obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
 
-		screen.fill("black")
+        pygame.display.flip()
 
-		for obj in drawable:
-			obj.draw(screen)
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
-		pygame.display.flip()
-
-		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
